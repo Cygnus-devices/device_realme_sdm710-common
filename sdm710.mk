02b1764755dee02a8d0573debd12e08a3690fe7f
MadhavSaladi <baaswanthmadhav@gmail.com>
2022-05-31 22:00:21 +0530

    sdm710-common: Update Bluetooth blobs
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -146,6 +146,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0 \
+    android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.0-impl:32 \
     audio.bluetooth.default \
     BluetoothResCommon \
